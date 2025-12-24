a77850f44cb610beaab688c2ae5c2ed018bccba0
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-12-24 11:43:35 +0900

    Change behavior of <C-u> and <C-d> to overscroll

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function feedkeys(keys)
+  local term = vim.api.nvim_replace_termcodes(keys, true, false, true)
+  vim.api.nvim_feedkeys(term, "n", false)
+end
+
+vim.keymap.set("n", "<C-d>", function()
+  local half = math.floor(vim.api.nvim_win_get_height(0) / 2)
+  for _ = 1, half do
+    feedkeys("<C-e>")
+  end
+end, { silent = true })
+
+vim.keymap.set("n", "<C-u>", function()
+  local half = math.floor(vim.api.nvim_win_get_height(0) / 2)
+  for _ = 1, half do
+    feedkeys("<C-y>")
+  end
+end, { silent = true })
