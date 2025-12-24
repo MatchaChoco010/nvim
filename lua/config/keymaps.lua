24d3da612113a7eb055d2141ed78dec562a4693c
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-12-24 15:19:24 +0900

    Add copilot and some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("t", "<C-]>", [[<C-\><C-n>]])
+vim.keymap.set("t", "<C-]>", [[<C-\><C-n>]], { desc = "escape from terminal" })
+
+vim.keymap.set("n", "<C-/>", "gcc", { remap = true, desc = "Line Comment Toggle" })
+
+vim.keymap.set("n", "<M-t>", function()
+  require("snacks.terminal").toggle()
+end, { desc = "Toggle terminal" })
+vim.keymap.set("t", "<M-t>", function()
+  require("snacks.terminal").toggle()
+end, { desc = "Toggle terminal" })
